a1903b1a553a80b8f21d33817c03dfc388b0aef0
bhavigoyal2000 <136422326+bhavigoyal2000@users.noreply.github.com>
2025-11-12 18:16:43 +0530

    updated chnages

--- blocks/calendar/calendar.js
+++ blocks/calendar/calendar.js
@@ -28,38 +28,26 @@ export default async function decorate(block) {
   const calendar = new window.FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth',
     headerToolbar: {
-      left: 'prev',
+      left: 'prev,next today',
       center: 'title',
-      right: 'next',
+      right: ''
     },
     selectable: true,
     editable: false,
     height: 'auto',
-    contentHeight: 'auto',
     dateClick(info) {
       console.log(`Selected date: ${info.dateStr}`);
       document.dispatchEvent(
-        new CustomEvent('calendar:dateSelected', { detail: { date: info.dateStr } }),
+        new CustomEvent('calendar:dateSelected', { detail: { date: info.dateStr } })
       );
     },
     events: [
       {
         title: 'Team Meeting',
-        start: new Date().toISOString().split('T')[0],
-      },
-    ],
+        start: new Date().toISOString().split('T')[0]
+      }
+    ]
   });
 
   calendar.render();
-
-  const safetyStyle = document.createElement('style');
-  safetyStyle.textContent = `
-    .block.calendar .fc table,
-    .block.calendar .fc td,
-    .block.calendar .fc th {
-      background: #fff !important;
-      color: #333 !important;
-    }
-  `;
-  document.head.appendChild(safetyStyle);
 }
