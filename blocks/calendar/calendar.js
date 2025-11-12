e40541be62fe4cc924c1250f60bfba363501b9fc
bhavigoyal2000 <136422326+bhavigoyal2000@users.noreply.github.com>
2025-11-12 18:30:18 +0530

    changes

--- blocks/calendar/calendar.js
+++ blocks/calendar/calendar.js
@@ -28,25 +28,19 @@ export default async function decorate(block) {
   const calendar = new window.FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth',
     headerToolbar: {
-      left: 'prev,next',
+      left: 'prev',
       center: 'title',
-      right: 'next',
+      right: 'next'
     },
+    height: 'auto',
     selectable: true,
     editable: false,
-    height: 'auto',
     dateClick(info) {
       console.log(`Selected date: ${info.dateStr}`);
       document.dispatchEvent(
         new CustomEvent('calendar:dateSelected', { detail: { date: info.dateStr } }),
       );
     },
-    events: [
-      {
-        title: 'Team Meeting',
-        start: new Date().toISOString().split('T')[0],
-      },
-    ],
   });
 
   calendar.render();
