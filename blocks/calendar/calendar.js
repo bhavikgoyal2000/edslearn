4a34c09c3723844ea38044888c3d2e29a8042510
bhavigoyal2000 <136422326+bhavigoyal2000@users.noreply.github.com>
2025-11-12 18:18:06 +0530

    nvjbfjvf

--- blocks/calendar/calendar.js
+++ blocks/calendar/calendar.js
@@ -28,9 +28,9 @@ export default async function decorate(block) {
   const calendar = new window.FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth',
     headerToolbar: {
-      left: 'prev,next today',
+      left: 'prev,next',
       center: 'title',
-      right: ''
+      right: 'next',
     },
     selectable: true,
     editable: false,
@@ -38,15 +38,15 @@ export default async function decorate(block) {
     dateClick(info) {
       console.log(`Selected date: ${info.dateStr}`);
       document.dispatchEvent(
-        new CustomEvent('calendar:dateSelected', { detail: { date: info.dateStr } })
+        new CustomEvent('calendar:dateSelected', { detail: { date: info.dateStr } }),
       );
     },
     events: [
       {
         title: 'Team Meeting',
-        start: new Date().toISOString().split('T')[0]
-      }
-    ]
+        start: new Date().toISOString().split('T')[0],
+      },
+    ],
   });
 
   calendar.render();
