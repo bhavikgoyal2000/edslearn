b5e5321a3d1db31d463c5523acc6dd372e4d1b5c
bhavigoyal2000 <136422326+bhavigoyal2000@users.noreply.github.com>
2025-11-12 17:40:23 +0530

    intend

--- blocks/calendar/calendar.js
+++ blocks/calendar/calendar.js
@@ -57,15 +57,15 @@ export default async function decorate(block) {
 
   calendar.render();
   setTimeout(() => {
-  const style = document.createElement('style');
-  style.textContent = `
-    .block.calendar .fc table *,
-    .block.calendar .fc td,
-    .block.calendar .fc th {
-      background: #fff !important;
-      color: #333 !important;
-    }
-  `;
-  document.head.appendChild(style);
-}, 100);
+    const style = document.createElement('style');
+    style.textContent = `
+      .block.calendar .fc table *,
+      .block.calendar .fc td,
+      .block.calendar .fc th {
+        background: #fff !important;
+        color: #333 !important;
+      }
+    `;
+    document.head.appendChild(style);
+  }, 100);
 }
