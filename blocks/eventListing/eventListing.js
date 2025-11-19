a3f9c8e2bddc8a738bf2b038786f797de5af5b93
bhavigoyal2000 <136422326+bhavigoyal2000@users.noreply.github.com>
2025-11-19 12:29:45 +0530

    event listing

--- blocks/eventListing/eventListing.js
+++
@@ -1,6 +0,0 @@
-export default function decorate(block) {
-  const wrapper = document.createElement('section');
-  wrapper.classList.add('eventlisting-wrapper');
-  block.textContent = 'cdbccdcdb';
-  block.appendChild(wrapper);
-}
